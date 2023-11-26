54837ac8595cede535c1311dce083a406c71a6fb
Ash <s.ashwin23@lpu.in>
2023-11-26 16:24:02 +0530

    loved it but cant understand mission quit see you ni8

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+from flask import url_for
 from flask import Flask
-
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "Hello World"
+    return 'index'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/login')
+def login():
+    return 'login'
 
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
+app.run(debug=True)
